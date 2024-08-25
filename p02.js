3cf1d07eb3c9f0b466f8429ef858328ba3613568
hanzalagithub <ahmedhanzala2020@gmail.com>
2024-08-25 13:18:48 +0500

    day02

---
+++ p02.js
@@ -0,0 +1,29 @@
+// find even number filter method
+// var arr  = [0,1,2,3,4,5,6,7,8,9]
+// var evenNums = arr.filter(number => number %2===0)
+// console.log(evenNums);
+
+// find odd number filter method
+// var arr  = [0,1,2,3,4,5,6,7,8,9]
+// var oddNums = arr.filter(number => number %2===1)
+// console.log(oddNums);
+
+//---------for loop method
+// var arr = [1,2,4,5,6,7,8,9];
+// // var evenNums = []
+// for (var i=0 ; i < arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         // evenNums.push(arr[i])
+//         console.log(arr[i]);
+
+//     }
+
+// } 
+
+//---------forEach loop method
+// var arr = [1,2,4,5,6,7,8,9]
+// arr.forEach(function(number){
+// if(number % 2 ===0){
+//     console.log(number);
+// }
+// })
