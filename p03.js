f7af2e23ff2b19972f92e3d6dd78ff53e978694b
hanzalagithub <ahmedhanzala2020@gmail.com>
2024-08-26 16:32:52 +0500

    day03

---
+++ p03.js
@@ -0,0 +1,53 @@
+//------ finding minimum numbers
+
+// var arr = [12,41,65,1,5]
+// var min = Math.min.apply(null,arr);
+// console.log(min);
+
+//------finding 2nd min number
+
+// var arr = [12,41,65,1,5]
+// var min = Math.min.apply(null,arr);
+// var secondMin = Math.min.apply(null,arr.filter(i => i != min));
+// //------third min
+// var thirdMin = Math.min.apply(null,arr.filter(i => i !==min && i !== secondMin))
+// console.log(thirdMin);
+
+// var arr = [12,41,65,1,5]
+// var min = Math.min.apply(null,arr)
+// console.log(min);
+// arr.splice(arr.indexOf(min),1)
+// var min2nd = Math.min.apply(null,arr)
+// console.log(min2nd);
+
+//------ finding max number in array
+//  var arr = [12,41,65,1,5]
+//  var max = Math.max.apply(null,arr);
+//  console.log(max);
+
+//------ findind 2nd most max number
+
+// var arr = [12,41,65,1,5]
+// var max = Math.max.apply(null,arr);
+// console.log(max);
+// var secondMax = Math.max.apply(null,arr.filter(i => i != max));
+// console.log(secondMax);
+
+//-----finding third number
+
+// var thirdMax = Math.max.apply(null,arr.filter(i => i !== max && i !==secondMax )); 
+// console.log(thirdMax);
+
+// var arr = [12,41,65,1,5]
+// var max = Math.max.apply(null,arr);
+// console.log(max);
+// arr.splice(arr.indexOf(max),1)
+// var max2nd = Math.max.apply(null,arr);
+// console.log(max2nd);
+
+
+
+
+
+
+
---
+++ p04.js
@@ -0,0 +1,29 @@
+// ------ finding sum
+
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = numbers.reduce((acc, current) => acc + current);
+// console.log(sum);
+
+// using for loop
+
+// var arr = [1,2,3,4,5]
+// var sum = 0;
+// for(i=0 ; i < arr.length ; i++){
+//     sum += arr[i];
+// }
+// console.log(sum); 
+
+//----using forEach
+
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = 0;
+// numbers.forEach(num => sum += num);
+// console.log(sum);
+
+// ------- finding average number
+
+// let numbers = [1, 2, 3, 4, 5];
+// let summ = 0;
+// numbers.forEach(num => summ += num);
+// let average = summ / numbers.length;
+// console.log(average);
