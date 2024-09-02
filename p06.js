102e5f36d0ca94b562631ca2f534fe1e800a0bef
hanzalagithub <ahmedhanzala2020@gmail.com>
2024-09-02 16:43:19 +0500

    DAY 6

---
+++ p06.js
@@ -0,0 +1,21 @@
+//----------find two consecuative same number
+
+// var arr = [1,1,6,8,4,675,2,1,2,3,3]
+// for (var i=1; i<arr.length; i++) {
+//     if (arr[i] === arr[i-1]) {
+//         console.log(`we have that same match = ${arr[i]}`);
+//     }
+// }
+
+// var arr=[1,2,2,3,4,5,5,6,7,7];
+// for(var i=1; i<arr.length; i++){
+//     if(arr[i]===arr[i-1]){
+//         console.log(`we have that same match = ${arr[i]}`)
+//     }
+// };
+
+// /-------11- loop timeout
+
+// for (let i = 1; i <= 5; i++) {
+//     setTimeout( () => console.log(i), 0);
+// }
---
+++ p07.js
@@ -0,0 +1,25 @@
+//--------------- reverse method
+// var arr = [1,2,3];
+// var reversed = arr.reverse();
+// console.log(reversed); 
+// console.log(arr);
+
+//-------------- for loop method
+
+// var arr = [1,2,3];
+// var reversed = [];
+// for(var i=arr.length-1; i>=0;i--){
+//     reversed.push(arr[i]);
+
+
+
+// }
+
+// console.log(reversed);
+
+//------------ swap number
+// var a=1;
+// var b=2;
+
+// [a,b]=[b,a]
+// console.log(a,b);
