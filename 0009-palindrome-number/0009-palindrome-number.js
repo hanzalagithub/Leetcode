12ae861d927395d50ff285b5193576f3d8570049
Ahmed hanzala <134609073+hanzalagithub@users.noreply.github.com>
2025-01-14 19:15:11 +0500

    Time: 17 ms (10.66%), Space: 59.1 MB (27.69%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x<0) return false;
+    let str = x.toString();
+    return str===str.split('').reverse().join('');
+
+};
+let x = 121;
+console.log(isPalindrome(x));
