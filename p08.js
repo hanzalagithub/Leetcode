b0a12adec41dd0d959c69adaddc0b760b00c4dd9
Ahmed Hanzala <ahmedhanzala2020@gmail.com>
2025-01-26 15:19:09 +0500

    another one

---
+++ p08.js
@@ -0,0 +1,85 @@
+// reverse the array
+// let arr = [1, 2, 3, 4, 5];
+// let reversedArr = [];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     reversedArr.push(arr[i]);
+// }
+// console.log(reversedArr);
+
+// let str = 'madam';
+// let reversedStr = '';
+// for (let i = str.length - 1; i >= 0; i--) {
+//     if(reversedStr += str[i]){
+//         console.log('this is palindrome');
+//     }else{
+//         console.log('this is not palindrome');
+//     }
+
+// }
+// console.log(reversedStr);
+
+// let arr = [1, 2, 3, 3, 4, 5];
+// let count = 0;
+// for (let element of arr){
+//     if(element === 3){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// let arr = [1, 2, 3, 3, 4, 5];
+// let duplicateArr = [];
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[i] === arr[j]) {
+//             duplicateArr.push(arr[i]);
+//             count++;
+//         }
+//     }
+// }
+// console.log(duplicateArr);
+
+
+// let arr = [1, 2, 3, 3, 4, 5,5];
+// let duplicateArr = [];
+// let count = 0;
+// for(let i=0; i<arr.length; i++){
+//     for(let j = i+1; j < arr.length; j++){
+//         if(arr[i]===arr[j]){
+//             duplicateArr.push(arr[i]);
+//             count++;
+//         }
+//     }
+// }
+// console.log(duplicateArr);
+
+// let str = 'geeksforgeeks';
+// let charCount = {};
+// for (let char of str){
+//     if(charCount[char]){
+//         charCount[char]++;
+
+
+//     }
+//     else{
+//         charCount[char]=1;
+//     }
+// }
+// console.log(charCount);
+
+// let arr = [3,5,1,2,4];
+// arr.sort((a,b)=>a-b);
+// console.log(arr);
+
+//Find the second largest and second smallest element in an array.
+// let arr = [3, 5, 1, 2, 4];
+// arr.sort((a, b) => a - b);
+// console.log(arr[1], arr[arr.length - 2]);
+
+// let arr = [0, 1, 0, 3, 12]
+// arr.sort((a,b)=>a-b);
+// console.log(arr);
+// arr.reverse();
+
+// console.log(arr);
